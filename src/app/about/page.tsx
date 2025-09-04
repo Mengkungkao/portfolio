27d8bf2985c81b4d2f5124b328b99c63c2bb13e8
M.K. <77830467+Mengkungkao@users.noreply.github.com>
2025-09-04 08:31:00 +0000

    updateerror

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -202,7 +202,7 @@ export default function About() {
                     <Text variant="body-default-s" onBackground="brand-weak" marginBottom="m">
                       {experience.role}
                     </Text>
-                    <Column as="ul" gap="10">
+                    <Column as="ul" gap="12">
                       {experience.achievements.map((achievement: JSX.Element, index: number) => (
                         <Text
                           as="li"
